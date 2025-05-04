0eb450dc724a6ef49f6a63adb913a184610d8c54
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-05-04 18:21:14 -0300

    Agrego resolución del ejercicio 7

---
+++ 04-archivos/ex-07.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Crear un script de Bash que imprima en un archivo llamado “ordenados.txt” 
+# los nombres de todos los archivos del directorio actual ordenados según sus tamaños.
+
+ls -lSr > "ordenados.txt"
