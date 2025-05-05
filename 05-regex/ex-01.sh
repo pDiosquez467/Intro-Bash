6f2d5bb9d10f67fc98e9ccfca9962bbbd015c739
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-05-05 10:34:58 -0300

    Agrego la resolución del ejercicio 1

---
+++ 05-regex/ex-01.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Encontrar la expresión regular de Regex que devuelve:
+
+#     Las ocurrencias de la letra s: s
+
+#     Las ocurrencias de un caracter, seguido de la letra o (ej. to): .o
+#     Todos los dígitos en el archivo: [0-9]
+#     Las ocurrencias del punto (.): \.
+#     Las ocurrencias de la letra c al comienzo de la palabra: \b[cC]
+#     Las palabras que comienzan con la letra p (no es necesario incluir las mayúsculas): \bp[a-z]*
+
+# ¿Qué devuelven las siguientes expresiones regulares?
+
+#     ^L          --> La línea comienza con L
+#     !$          --> La línea termina con un signo de exclamación 
+#       (espacio) --> Es simplemente un carácter de espacio, igual que presionar la barra espaciadora.
+#     [A-Z1-3]    --> Coincide con cualquier letra mayúscula de la A a la Z, o los números 1, 2 o 3.
